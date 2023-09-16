f6f4a75f56a819d4d0dbfe6c853d7e433717d7bc
itsMeVela <velaputri681@gmail.com>
2023-09-17 02:53:46 +0700

    sign in whit

--- index.php
+++ index.php
@@ -48,9 +48,73 @@
                     <div class="card-body">
                         <h1 class="card-title text-center mb-3">Welcome</h1>
                         <h4 class="card-title mb-2 text-center">Hey, Enter your details get sign in <br> to your account</h4>
-                        <div class="input-group mb-3" style="margin-top: 50px;">
-                            <input type="text" class="form-control p-3" placeholder="Enter Email/Phone No" aria-label="Enter Email/Phone No" aria-describedby="basic-addon2">
-                            <span class="input-group-text p-3" id="basic-addon2">@</span>
+                        <div class="d-flex justify-content-center">
+                            <div class="col-10">
+                                <div class="input-group mb-3" style="margin-top: 50px;">
+                                    <input type="text" class="form-control p-3" placeholder="Enter Email/Phone No" aria-label="Enter Email/Phone No" aria-describedby="basic-addon2">
+                                    <span class="input-group-text p-3 " id="basic-addon2">@ </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <div class="col-10">
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control p-3" placeholder="Password" aria-label="Password" aria-describedby="button-addon2">
+                                    <button class="btn btn-outline-secondary p-3" type="button" id="button-addon2">Hide</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link mx-3" aria-current="page" href="#">
+                                        <h6 class="mt-2" style="margin-left: 7%;">heaving trouble in sign in?</h6>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <div class="col-10">
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-warning p-3 mt-3" type="button">Sign In</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <h6 class="text-center mt-5">-- Or Sign In With --</h6>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="row">
+                                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+                                        <div class="col-6">
+                                            <svg  xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 488 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                                <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+                                            </svg>
+                                        </div>
+                                        <div class="col-6">
+                                            <h6>Google</h6>
+                                        </div>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+                                <svg style="margin-button: 35%; " xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
+                                </svg>
+                                <h6>Apple</h6>
+                            </button>
+                        </div>
+                        <div class="col-4">
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z" />
+                                </svg>
+                                <h6>Facebook</h6>
+                            </button>
                         </div>
                     </div>
                 </div>
