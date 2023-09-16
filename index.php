92f3b584179925cd949d1346e1bb781153d5ee2c
itsMeVela <velaputri681@gmail.com>
2023-09-17 01:43:17 +0700

    card

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<style>
+    body {
+        background-color: rgb(232, 232, 232);
+    }
+</style>
+
+<body>
+
+    <div class="container-fluid mt-4 ">
+        <div class="row">
+            <div class="col-10">
+                <image src="assets/img/Favel.png" class="d-inline-block  align-text-top " alt="" style="width: 100px; height: 35px; margin-left: 4%; ">
+                    <div class="col-2 rounded mt-3 " style="border: 1px solid #000; margin-left: 4%; ">
+                    </div>
+                    <div style="margin-left: 4%;">
+                        <p> g tau mau di isi apa, tulisannya g jelas</p>
+                    </div>
+            </div>
+            <div class="col-2">
+                <div class="row">
+                    <div class="col-5">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link mx-3" aria-current="page" href="#"> SIGN UP</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-7">
+                        <button type="button" class="btn btn-warning btn-lg">Warning</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="d-flex justify-content-center">
+            <div class="d-flex align-items-center" style="height: 85vh;">
+                <div class="card rounded-5" style="width: 43.75rem;">
+                    <div class="card-body">
+                        <h1 class="card-title text-center mb-3">Welcome</h1>
+                        <h4 class="card-title mb-2 text-center">Hey, Enter your details get sign in <br> to your account</h4>
+                        <div class="input-group mb-3" style="margin-top: 50px;">
+                            <input type="text" class="form-control p-3" placeholder="Enter Email/Phone No" aria-label="Enter Email/Phone No" aria-describedby="basic-addon2">
+                            <span class="input-group-text p-3" id="basic-addon2">@</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</html>
