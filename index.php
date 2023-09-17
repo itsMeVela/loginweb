981148cebe9b2f8fe5956d4a05757aa2c2c14cd6
itsMeVela <velaputri681@gmail.com>
2023-09-17 14:37:40 +0700

    perbaikan 1

--- index.php
+++ index.php
@@ -89,13 +89,13 @@
                             <div class="col-4">
                                 <div class="row">
                                     <button class="btn btn-outline-secondary" type="button" id="button-addon2">
-                                        <div class="col-6">
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 488 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                        <div class="col-6" style="width: 80px;">
+                                            <svg  style=" margin-right: 95px; margin-top: 15px;" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 488 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                                                 <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
                                             </svg>
                                         </div>
                                         <div class="col-6">
-                                            <h6>Google</h6>
+                                            <h6 >Google</h6>
                                         </div>
                                     </button>
                                 </div>
@@ -137,8 +137,8 @@
                 </div>
             </div>
         </div>
-        <div >
-            <p class="text-center " style="margin-button: 95%;"> copyright@velaputri 2023 | g tau g kebaca  </p><!--  gak bisa ke atas an lagi -->
+        <div>
+            <p class="text-center" style="margin-bottom: 1%;"> copyright@velaputri 2023 | g tau g kebaca  </p><!--  gak bisa ke atas an lagi -->
         </div>
     </div>
     </div>
